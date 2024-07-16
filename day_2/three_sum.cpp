e0781053a7bb674c73cf45d1173d219fd7e192a5
Kavan Gondalia <kavan155gondalia@gmail.com>
2024-07-16 14:00:56 +0530

    da_2: modify three sum

--- day_2/three_sum.cpp
+++ day_2/three_sum.cpp
@@ -1,23 +1,30 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        int n = nums.size();
         sort(nums.begin(), nums.end());
-
-        unordered_map<int, int> mp;
-        for (int i = 0; i<n; i++) mp[nums[i]] = i;
-
+        int n = nums.size();
         vector<vector<int>> result;
-        for (int i = 0; i<nums.size()-2; i++) {
-            for (int j = i+1; j<nums.size()-1; j++) {
-                int sum = -1*(nums[i] + nums[j]);
-                if (mp.count(sum) && mp[sum] > j) {
-                    result.push_back({nums[i], nums[j], sum});
+
+        for (int i = 0; i<n; i++) {
+            if (i>0 && nums[i] == nums[i-1]) continue;
+            int j = i+1;
+            int k = n-1;
+
+            while (j<k) {
+                int sum = nums[i] + nums[j] + nums[k];
+                if (sum == 0) {
+                    result.push_back({nums[i], nums[j], nums[k]});
+                    j++, k--;
+                    while (j<k && nums[j] == nums[j-1]) j++;
+                    while (j<k && nums[j] == nums[k+1]) k--;
+                } else if (sum < 0) {
+                    j++;
+                } else {
+                    k--;
                 }
-                j = mp[nums[j]];
             }
-            i = mp[nums[i]];
         }
+
         return result;
     }
 };
